982048b923aa3a9714b0d0d2e316d6e9c5c2c752
Lautaro Marguery <l.e.marguery@gmail.com>
2025-09-01 00:39:49 -0300

    appId modificado.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.B',
   appName: 'AppL.M.PrimerEtapa',
   webDir: 'dist',
   bundledWebRuntime: false,
